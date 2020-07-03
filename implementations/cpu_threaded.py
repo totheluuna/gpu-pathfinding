2ac74b70c705bb36521a6d78bfb7486737c4f847
Luna Dilangalen <dadilangalen@gmail.com>
2020-07-04 00:45:13 +0800

    updated cpu_threaded

--- implementations/cpu_threaded.py
+++ implementations/cpu_threaded.py
@@ -1,20 +1,12 @@
-from search_algorithms import a_star
-from search_algorithms import breadth_first 
-from search_algorithms import djikstra
-from helper import reconstructPath
-
-def pathfinder(algorithm='a_star', graph=None, start=None, goal=None):
+from algorithms import a_star
+from utilities import helper
+def CPUThreaded(algorithm='a_star', graph=None, start=None, goal=None):
     path = []
     cameFrom = {}
     costSoFar = {}
     if (algorithm == 'a_star'):
         cameFrom, costSoFar = a_star.search(graph, start, goal)
-        path = reconstructPath(cameFrom, start, goal) 
-    elif (algorithm == 'breadth_first'):
-        cameFrom = breadth_first.search(graph, start, goal)
-    elif (algorithm == 'djikstra'):
-        cameFrom, costSoFar = djikstra.search(graph, start, goal)
-        path = reconstructPath(cameFrom, start, goal) 
+        path = helper.reconstructPath(cameFrom, start, goal) 
     else:
         print("No implementation of search algorithm")
     # print('Came from (Hashmap): ', cameFrom)

5868d53767894edd45aa9f12fe5b26e9fb2a962f
Luna Dilangalen <dadilangalen@gmail.com>
2020-11-25 15:32:24 +0800

    debugging numba implementation

--- implementations/gpu_pathfinder.py
+++ implementations/gpu_pathfinder.py
@@ -8,9 +8,9 @@ TPB = 16
 
 @cuda.jit
 def heuristic(a, b):
-        x1, y1 = a
-        x2, y2 = b
-        return abs(x1-x2) + abs(y1-y2)
+    x1, y1 = a
+    x2, y2 = b
+    return abs(x1-x2) + abs(y1-y2)
 
 @cuda.jit
 def GPUPathfinder(grid, start, goal, hArray):

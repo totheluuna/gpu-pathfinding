ae7de21ad49d5cfabaf0e9b887d4b81d18175ab7
Luna Dilangalen <dadilangalen@gmail.com>
2020-11-25 17:42:43 +0800

    debugging numba implementation

--- implementations/gpu_pathfinder.py
+++ implementations/gpu_pathfinder.py
@@ -14,16 +14,6 @@ def heuristic(x1, y1, x2, y2):
 
 @cuda.jit
 def GPUPathfinder(grid, start, goal, hArray):
-    # path = []
-    # parents = {}
-    # FCost = {}
-    # if algorithm == 'a_star':
-    #     parents, FCost = a_star_v3.search(graph, start, goal)
-    #     path = helper.reconstructPathV2(parents, start, goal)
-    # else:
-    #     print("No implementation of the search algorithm")
-    # return FCost, path
-
     x, y = cuda.grid(2)
 
     tx = cuda.threadIdx.x
@@ -35,5 +25,10 @@ def GPUPathfinder(grid, start, goal, hArray):
         if grid[x, y] != 0:
             openList = pq.PriorityQueue()
             hArray[x, y] = heuristic(x, y, goal_x, goal_y)
+
+@jit
+def CPUPathfinder(grid, start, goal, path):
+    print(grid, start, goal, path)
+
 
 

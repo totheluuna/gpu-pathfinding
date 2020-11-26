64480f2191fd14dc04a511adf9282e193a9ba5a9
Luna Dilangalen <dadilangalen@gmail.com>
2020-11-26 20:40:33 +0800

    debugging for jit compilation

--- implementations/cpu_pathfinder_numba.py
+++ implementations/cpu_pathfinder_numba.py
@@ -135,12 +135,12 @@ def search(grid, start, goal, parentHash, FValue):
     width, height = grid.shape
 
     openList = List()
-    openList.append((9999, (-1, -1)))
-    openListEntryFinder = {(9999,9999) : (9999, (-1, -1))}
+    openList.append((-9999, (-1, -1)))
+    openListEntryFinder = {(9999,9999) : (-9999, (-1, -1))}
 
     closedList = List()
-    closedList.append((9999, (-1, -1)))
-    closedListEntryFinder = {(9999,9999) : (9999, (-1, -1))}
+    closedList.append((-9999, (-1, -1)))
+    closedListEntryFinder = {(9999,9999) : (-9999, (-1, -1))}
     
     GValue = np.zeros((width, height), dtype=np.int32)
     HValue = np.zeros((width, height), dtype=np.int32)

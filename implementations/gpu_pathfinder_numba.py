206260d32f44779fae44584416aac2190c9de6b8
Luna Dilangalen <dadilangalen@gmail.com>
2020-11-30 14:25:38 +0800

    debugging cuda jit compiled pathfinder

--- implementations/gpu_pathfinder_numba.py
+++ implementations/gpu_pathfinder_numba.py
@@ -207,7 +207,7 @@ def popFromPQ(elements, entryFinder):
 @cuda.jit
 def GPUPathfinder(grid, start, goal, hArray, parents, cost):
     x, y = cuda.grid(2)
-    temp = [1,2]
+    temp = (1,2)
     tx = cuda.threadIdx.x
     ty = cuda.threadIdx.y
     bpg = cuda.gridDim.x    # blocks per grid

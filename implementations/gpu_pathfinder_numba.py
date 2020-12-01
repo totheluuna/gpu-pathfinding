c890b857c8e28368ec16153bd097b5e6be7b16b9
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-01 21:07:34 +0800

    debugging cuda jit compiled pathfinder

--- implementations/gpu_pathfinder_numba.py
+++ implementations/gpu_pathfinder_numba.py
@@ -199,7 +199,7 @@ def search(grid, start, goal, parentHash, FValue):
 
 @cuda.jit(device=True)
 def search_gpu(grid, start, goal, parents, cost, gArray, hArray, openArray, closedArray):
-    hArray[start] += 69
+    hArray[start] = heuristic(start, goal)
 
 # functions for priority queue
 @cuda.jit(device=True)

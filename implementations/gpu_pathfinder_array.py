b43d04f46158b69a11e11594213a02786f4c26e0
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-04 20:56:36 +0800

    debugging gpu implementation to mainly use arrays

--- implementations/gpu_pathfinder_array.py
+++ implementations/gpu_pathfinder_array.py
@@ -230,12 +230,12 @@ def GPUPathfinder(grid, start, goal, open, closed, parents, cost, g, h, UNEXPLOR
     width, height = cost.shape
 
     # create copies of all arrays expected to have changing values
-    open_copy = cuda.local.array(shape=open.shape, dtype=int32)
-    closed_copy = cuda.local.array(shape=closed.shape, dtype=int32)
-    parents_copy = cuda.local.array(shape=parents.shape, dtype=int32)
-    cost_copy = cuda.local.array(shape=cost.shape, dtype=int32)
-    g_copy =  cuda.local.array(shape=g.shape, dtype=int32)
-    neighbors_copy = cuda.local.array(shape=neighbors.shape, dtype=int32)
+    open_copy = cuda.local.array(shape=(width, height), dtype=int32)
+    closed_copy = cuda.local.array(shape=(width, height), dtype=int32)
+    parents_copy = cuda.local.array(shape=(width, height), dtype=int32)
+    cost_copy = cuda.local.array(shape=(width, height), dtype=int32)
+    g_copy =  cuda.local.array(shape=(width, height), dtype=int32)
+    neighbors_copy = cuda.local.array(shape=(neighbors.shape[0], neighbors.shape[1]), dtype=int32)
 
     tx = cuda.threadIdx.x
     ty = cuda.threadIdx.y

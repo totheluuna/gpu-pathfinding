09e106650b8d7f644118f6e403f77a47dc253de2
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-04 15:56:52 +0800

    debugging gpu implementation to mainly use arrays

--- implementations/gpu_pathfinder_array.py
+++ implementations/gpu_pathfinder_array.py
@@ -234,9 +234,9 @@ def GPUPathfinder(grid, start, goal, open, closed, parents, cost, g, h, UNEXPLOR
     # print(bpg)
     if x < grid.shape[0] and y < grid.shape[1]:
         # do the search for as many times as number of tiles in the grid
-        search(grid, start, goal, open, closed, parents, cost, g, h, UNEXPLORED, neighbors)
+        # search(grid, start, goal, open, closed, parents, cost, g, h, UNEXPLORED, neighbors)
         # h[x,y] = heuristic((x,y), goal)
-        # counter += 1
+        counter += 1
         # cuda.syncthreads() 
 
 def main():

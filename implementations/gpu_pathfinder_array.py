5492703b755ff6b485d8fdaaf644e8c18cdd1527
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-04 15:24:41 +0800

    debugging gpu implementation to mainly use arrays

--- implementations/gpu_pathfinder_array.py
+++ implementations/gpu_pathfinder_array.py
@@ -240,7 +240,7 @@ def GPUPathfinder(grid, start, goal, open, closed, parents, cost, g, h, UNEXPLOR
 
 def main():
     # create grid from image dataset
-    scale_factor = 7 # scales to a power of 2
+    scale_factor = 4 # scales to a power of 2
     dim = (int(math.pow(2, scale_factor)), int(math.pow(2, scale_factor)))
     UNEXPLORED = int(math.pow(2, (scale_factor*2)))
     grid = np.zeros(dim, dtype=np.int32)

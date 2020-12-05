efaf044629dc9bba49cf6eef9c09905fc2e9911b
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-05 11:47:48 +0800

    debugging gpu implementation to mainly use arrays

--- implementations/gpu_pathfinder_array.py
+++ implementations/gpu_pathfinder_array.py
@@ -14,7 +14,7 @@ from numba import cuda, int32, typeof
 OPEN = 1
 CLOSED = 0
 
-scale_factor = 4 # scales to a power of 2
+scale_factor = 2 # scales to a power of 2
 dim = (int(math.pow(2, scale_factor)), int(math.pow(2, scale_factor)))
 UNEXPLORED = int(math.pow(2, (scale_factor*2)))
 

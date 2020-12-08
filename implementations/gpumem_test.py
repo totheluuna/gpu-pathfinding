ea523d4d8f3199d3ecf132b515617d7f4e07e425
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-08 20:26:46 +0800

    testing gpu memory

--- implementations/gpumem_test.py
+++ implementations/gpumem_test.py
@@ -20,8 +20,8 @@ def gpu_memory_test(arr):
     print(bx)
 
     shared_arr = cuda.shared.array(shape=(TPB, TPB), dtype=int32)
-    arr[tx + bx * dim_x , ty + by * dim_y] = bx
-    shared_arr[tx + bx * dim_x , ty + by * dim_y] = arr[tx + bx * dim_x , ty + by * dim_y]
+    arr[tx + (bx * dim_x) , ty + (by * dim_y)] = bx + by
+    shared_arr[tx + (bx * dim_x) , ty + (by * dim_y)] = arr[tx + (bx * dim_x) , ty + (by * dim_y)]
 
 def main():
     arr = np.zeros(shape=(8,8), dtype=np.int32)

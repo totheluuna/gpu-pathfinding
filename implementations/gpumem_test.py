0721f02bbd783c72a8b842b5f847a4491ea39af9
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-09 18:33:58 +0800

    testing gpu memory

--- implementations/gpumem_test.py
+++ implementations/gpumem_test.py
@@ -30,7 +30,7 @@ def gpu_memory_test(arr):
 
     for i in range(bpg):
         arr[tx + (bx * i) , ty + (by * i)] = i+1
-        cuda.syncthreads()
+        # cuda.syncthreads()
         shared_arr[tx + (bx * i) , ty + (by * i)] = arr[tx + (bx * i) , ty + (by * i)]
         cuda.syncthreads()
 

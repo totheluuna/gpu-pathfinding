a845ad6b372554b5ca49f00156ada33e9d4fbc15
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-10 17:08:44 +0800

    searching paths to goal for all tiles

--- implementations/gpu_pathfinder_array.py
+++ implementations/gpu_pathfinder_array.py
@@ -244,7 +244,7 @@ def GPUPathfinder(grid, start, goal, open, closed, parents, cost, g, h, neighbor
     # print(bpg)
     if x < grid.shape[0] and y < grid.shape[1]:
         # do the search for as many times as number of tiles in the grid
-        if passable(grid, (x,y)):
+        if passable(grid, (x,y)) and (x != goal[0] and y != goal[1]):
             search(x, y, grid, (x,y), goal, open[x,y], closed[x,y], parents[x,y], cost[x,y], g[x,y], h, neighbors[x,y])
 
 @cuda.jit

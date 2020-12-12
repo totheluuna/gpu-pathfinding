e5e11a4f1dc6ab0bde62f93b86efd24a9dd8f29a
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-12 13:19:51 +0800

    trying out local memory and shared memory

--- implementations/gpumem_test.py
+++ implementations/gpumem_test.py
@@ -32,7 +32,7 @@ def gpu_memory_test(arr):
     cuda.syncthreads()
     shared_arr = cuda.shared.array((TPB,TPB), int32)
     # shared_arr[tx,ty] = bx * dim_x + by
-    shared_arr[tx,ty] = arr[tx, ty]
+    shared_arr[tx,ty] = arr[tx * width + ty, ty * width + tx]
     cuda.syncthreads()
     # arr[tx,ty] = shared_arr[tx, ty]*2
     # cuda.syncthreads()

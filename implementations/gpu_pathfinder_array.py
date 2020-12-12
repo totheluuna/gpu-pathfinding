4452b1dbce0609afb290fa8fcc66c4a39b5a1e9a
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-12 17:00:32 +0800

    trying out local memory and shared memory on gpu pathfinder

--- implementations/gpu_pathfinder_array.py
+++ implementations/gpu_pathfinder_array.py
@@ -363,7 +363,7 @@ def GridDecompPath(grid, start, goal, parents, h, block):
             for i in range(TPB):
                 for j in range(TPB):
                     sum += shared_parents[i,j]
-            block[x,y] = sum
+            parents[x,y] = sum
             # search(x, y, shared_planning_block, (tx,ty), goal, local_open, local_closed, parents, local_cost, local_g, h, local_neighbors, block)
             # search(x, y, grid, (x,y), goal, local_open, local_closed, parents, local_cost, local_g, h, local_neighbors, block)
             # search(x, y, grid, (x,y), goal, open[x,y], closed[x,y], parents[x,y], cost[x,y], g[x,y], h, neighbors[x,y], block)

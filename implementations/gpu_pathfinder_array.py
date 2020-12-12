a53dc695e598e2b90f97653d7e5f2c63d6dfc747
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-12 15:28:25 +0800

    trying out local memory and shared memory on gpu pathfinder

--- implementations/gpu_pathfinder_array.py
+++ implementations/gpu_pathfinder_array.py
@@ -269,7 +269,6 @@ def GPUPathfinder(grid, start, goal, open, closed, parents, cost, g, h, neighbor
     ty = cuda.threadIdx.y
     bpg = cuda.gridDim.x    # blocks per grid
 
-    boundary_check = 
     # print(bpg)
     if x < grid.shape[0] and y < grid.shape[1]:
         # do the search for as many times as number of tiles in the grid

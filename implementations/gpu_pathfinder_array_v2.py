47145596d2f84822bf9bcd7af57f8e9cc54076ef
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-14 06:00:40 +0800

    trying out local start/goal approach

--- implementations/gpu_pathfinder_array_v2.py
+++ implementations/gpu_pathfinder_array_v2.py
@@ -547,16 +547,16 @@ def main():
         print(i)
         MapBlocks[blockspergrid, threadsperblock](blocked_guide_gpu[i], local_parents_gpu[i])
 
-    # parents = unblockshaped(local_parents, dim[0], dim[1])
-    # print(parents)
+    parents = unblockshaped(local_parents, dim[0], dim[1])
+    print(parents)
 
-    # # neighbors = cp.zeros((dim[0], dim[1], 8, 2), cp.int32)
-    # MapBlocks2[blockspergrid, threadsperblock](guide, parents, H_start)
-    # print(guide)
-    # print(parents)
-    # path = []
-    # reconstructPathV2(parents, start, goal, path)
-    # print(path)
+    # neighbors = cp.zeros((dim[0], dim[1], 8, 2), cp.int32)
+    MapBlocks2[blockspergrid, threadsperblock](guide, parents, H_start)
+    print(guide)
+    print(parents)
+    path = []
+    reconstructPathV2(parents, start, goal, path)
+    print(path)
     
 
 

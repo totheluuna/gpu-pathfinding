9a2bd569bf115160acdb44b6eb38eaa577951c82
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-14 06:15:53 +0800

    trying out local start/goal approach

--- implementations/gpu_pathfinder_array_v2.py
+++ implementations/gpu_pathfinder_array_v2.py
@@ -15,10 +15,10 @@ from numba import cuda, int32, typeof
 OPEN = 1
 CLOSED = 0
 
-scale_factor = 4 # scales to a power of 2
+scale_factor = 10 # scales to a power of 2
 dim = (int(math.pow(2, scale_factor)), int(math.pow(2, scale_factor)))
 UNEXPLORED = int(math.pow(2, (scale_factor*2)))
-TPB = 4
+TPB = 32
 
 # seed(42042042069)
 seed(42069)

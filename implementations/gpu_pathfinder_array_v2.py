e8632c8dc368487a811fd22f352fca092ec7d4ef
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-13 21:36:53 +0800

    trying out local start/goal approach

--- implementations/gpu_pathfinder_array_v2.py
+++ implementations/gpu_pathfinder_array_v2.py
@@ -398,7 +398,7 @@ def main():
     local_parents = np.empty(blocked_grid.shape, np.int32)
 
     # Simultaneous local search
-    SimultaneousLocalSearch[blockspergrid, threadsperblock](blocked_grid, local_start, local_goal, blocked_H_goal, blocked_H_start, local_parents, block))
+    SimultaneousLocalSearch[blockspergrid, threadsperblock](blocked_grid, local_start, local_goal, blocked_H_goal, blocked_H_start, local_parents, block)
 
     print('Something')
 

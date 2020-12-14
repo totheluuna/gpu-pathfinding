f984909f9093860e4a78af03799e37038ca9e933
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-14 08:13:00 +0800

    trying out local start/goal approach

--- implementations/gpu_pathfinder_array_v2.py
+++ implementations/gpu_pathfinder_array_v2.py
@@ -541,7 +541,8 @@ def main():
     print(local_parents.shape)
     for i in range(local_parents.shape[0]):
         print('%dth block: '%(i))
-        print(local_parents[i])
+        # print(local_parents[i])
+        print(blocked_grid[i])
         print()
     for i in range(local_parents.shape[0]):
         MapBlocks[blockspergrid, threadsperblock](blocked_guide[i], local_parents[i])

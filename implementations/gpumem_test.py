2506322982365724ba4c1dd47a960df017dbf376
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-16 22:33:34 +0800

    testing unreachable goal case

--- implementations/gpumem_test.py
+++ implementations/gpumem_test.py
@@ -4,10 +4,11 @@ from numba import cuda, int32
 import numpy as np
 import cupy as cp
 import math
+from skimage.util.shape import view_as_windows
 
 dim = (8,8)
 @cuda.jit
-def gpu_memory_test(arr, block, thread, shared_sum_arr, local_sum_arr, extended_arr):
+def gpu_memory_test(arr, block, thread, shared_sum_arr, local_sum_arr, padded_arr):
     x, y = cuda.grid(2)
     width, height = dim
     tx = cuda.threadIdx.x
@@ -36,7 +37,7 @@ def gpu_memory_test(arr, block, thread, shared_sum_arr, local_sum_arr, extended_
     # shared_arr[tx,ty] = arr[x, y]
     # cuda.syncthreads()
 
-    extended_arr[x+1,y+1] = arr[x,y]
+    padded_arr[x+1,y+1] = arr[x,y]
     cuda.syncthreads()
 
     # block[x , y] = bx * dim_x + by
@@ -76,7 +77,7 @@ def main():
     block = np.zeros(shape=dim, dtype=np.int32)
     shared_sum_arr = np.zeros(shape=dim, dtype=np.int32)
     local_sum_arr = np.zeros(shape=dim, dtype=np.int32)
-    extended_arr = np.zeros(shape=(dim[0]+2, dim[1]+2), dtype=np.int32)
+    padded_arr = np.zeros(shape=(dim[0]+2, dim[1]+2), dtype=np.int32)
     # arr_gpu = cp.zeros(shape=(8,8), dtype=cp.int32)
 
     w, h = arr.shape
@@ -89,7 +90,7 @@ def main():
     blockspergrid_y = math.ceil(arr.shape[1] / threadsperblock[1])
     blockspergrid = (blockspergrid_x, blockspergrid_y)
     print('blocks per grid: ', blockspergrid, '\nthreads per block: ', threadsperblock)
-    gpu_memory_test[blockspergrid, threadsperblock](arr, block, thread, shared_sum_arr, local_sum_arr, extended_arr)
+    gpu_memory_test[blockspergrid, threadsperblock](arr, block, thread, shared_sum_arr, local_sum_arr, padded_arr)
 
     # print('Array: ')
     # print(arr)
@@ -104,7 +105,9 @@ def main():
     # print(shared_sum_arr)
     # print('Local Sum Array: ')
     # print(local_sum_arr)
-    print(extended_arr)
+    print(padded_arr)
+    chunks = view_as_windows(padded_arr, (TPB+2, TPB+2))
+    print(chunks)
 
 if __name__ == "__main__":
     main()

56dd5953491c7b8b31126257ff9536c5c48d2e97
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-19 19:20:57 +0800

    trying out padded block approach

--- implementations/gpu_pathfinder_array_v3.py
+++ implementations/gpu_pathfinder_array_v3.py
@@ -403,7 +403,7 @@ def GridDecompSearch(grid, start, goal, h, block, parents, grid_blocks, guide_bl
         local_neighbors[i, 1] = 0
     cuda.syncthreads()
 
-    searchV2(local_grid, local_start, goal, local_open, local_closed, parents[x,y], local_cost, local_g, local_h, local_neighbors, local_block, local_guide)
+    # searchV2(local_grid, local_start, goal, local_open, local_closed, parents[x,y], local_cost, local_g, local_h, local_neighbors, local_block, local_guide)
     cuda.syncthreads()
 
     

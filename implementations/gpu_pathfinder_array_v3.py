4de0e9acb10572e4793fc665db8b3949fcd8b698
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-19 20:14:22 +0800

    trying out padded block approach

--- implementations/gpu_pathfinder_array_v3.py
+++ implementations/gpu_pathfinder_array_v3.py
@@ -267,13 +267,13 @@ def searchV2(grid, start, goal, open, closed, parents, cost, g, h, neighbors, bl
         current_x, current_y = getMinIndex(open)
         current = (current_x, current_y)
         # TODO: find actual current tile
-        actual_current = guide[current]
+        # actual_current = guide[current]
         # print(actual_current)
-        # if (current_x == goal_x and current_y == goal_y):
+        if (current_x == goal_x and current_y == goal_y):
         # TODO: change stop condition to: if actual current == goal or block[start] != block[current]
-        if (goal_1d_index == actual_current) or (block[start] != block[current]):
+        # if (goal_1d_index == actual_current) or (block[start] != block[current]):
         # or (block[current_x, current_y] != block[start_x, start_y]):
-            print(actual_current, goal_1d_index, block[start], block[current])
+            # print(actual_current, goal_1d_index, block[start], block[current])
             break
         getNeighbors(grid, current, neighbors)
         for next in neighbors:

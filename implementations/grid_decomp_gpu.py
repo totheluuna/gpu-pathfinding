d5e4b874416395ca393a856b7f0c3bd3b3169d57
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-23 17:13:10 +0800

    testing out new grid decomposition approach on gpu implementation

--- implementations/grid_decomp_gpu.py
+++ implementations/grid_decomp_gpu.py
@@ -267,7 +267,7 @@ def searchV2(grid, start, goal, open, closed, parents, cost, g, h, neighbors, bl
         current_x, current_y = getMinIndex(open)
         current = (current_x, current_y)
         actual_index = guide[current]
-        print(block[current], actual_index)
+        print(start_x, start_y, block[current], actual_index)
         if (actual_index == goal_1d_index) or (block[start] != block[current]):
             # print("\riterations: {}".format(counter), end='')
             counter_tile = counter

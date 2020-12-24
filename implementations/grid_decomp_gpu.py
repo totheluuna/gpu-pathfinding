b76fc931fd62ec30a23bd6a9b83da49975b44809
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-24 21:18:52 +0800

    modified gpu search to save last tile visited as established goal

--- implementations/grid_decomp_gpu.py
+++ implementations/grid_decomp_gpu.py
@@ -13,7 +13,7 @@ from timeit import default_timer as timer
 from numba import cuda, int32, typeof
 from skimage.util.shape import view_as_windows
 
-scale_factor = 5 # scales to a power of 2
+scale_factor = 7 # scales to a power of 2
 dim = int(math.pow(2, scale_factor)), int(math.pow(2, scale_factor))
 TPB = 4
 padded_TPB = TPB + 2

65ac6a3b8320f7fbab3cdefcbb64ad89e8e7cfef
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-24 15:36:53 +0800

    testing out new grid decomposition approach on gpu implementation

--- implementations/grid_decomp_gpu.py
+++ implementations/grid_decomp_gpu.py
@@ -18,8 +18,8 @@ dim = int(math.pow(2, scale_factor)), int(math.pow(2, scale_factor))
 TPB = 4
 padded_TPB = TPB + 2
 
-# UNEXPLORED = int(math.pow(2, (scale_factor*2)))
-UNEXPLORED = 9999999
+UNEXPLORED = int(math.pow(2, (scale_factor*2)))
+# UNEXPLORED = 9999999
 OPEN = 1
 CLOSED = 0
 

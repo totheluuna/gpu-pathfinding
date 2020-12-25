1e995cb1f8c13ba0f515925a5ae734b83501c09b
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-25 18:30:53 +0800

    rearranging functions

--- implementation_v2/main.py
+++ implementation_v2/main.py
@@ -8,26 +8,12 @@ import helper
 import config
 import numpy as np
 
-scale_factor = 4 # scales to a power of 2
-dim = int(math.pow(2, scale_factor)), int(math.pow(2, scale_factor))
-TPB = 4
-padded_TPB = TPB + 2
-
-UNEXPLORED = int(math.pow(2, (scale_factor*2)))
-# UNEXPLORED = 9999999
-OPEN = 1
-CLOSED = 0
-
 def test_func():
     print('scale factor: ', config.scale_factor)
     print('TPB: ', config.TPB)
     print('max value: ', config.UNEXPLORED)
 
 def main():
-    # global scale_factor
-    # global TPB
-    # global dim
-    # global UNEXPLORED
 
     parser = argparse.ArgumentParser(description='GPU Pathfinding')
     parser.add_argument('scale_factor', type=int, help='Scale factor (power of 2)')

720a419d124f9f43acf3b8305a48d682ae1d180d
Luna Dilangalen <dadilangalen@gmail.com>
2020-12-25 18:26:11 +0800

    rearranging functions

--- implementation_v2/globals.py
+++ implementation_v2/globals.py
@@ -1,10 +1,10 @@
-import config
+import configobj
 
-config.scale_factor = 4 # scales to a power of 2
-config.dim = int(math.pow(2, scale_factor)), int(math.pow(2, scale_factor))
-config.TPB = 4
-config.padded_TPB = TPB + 2
-config.UNEXPLORED = int(math.pow(2, (scale_factor*2)))
-# config.UNEXPLORED = 9999999
-config.OPEN = 1
-config.CLOSED = 0
+configobj.scale_factor = 4 # scales to a power of 2
+configobj.dim = int(math.pow(2, scale_factor)), int(math.pow(2, scale_factor))
+configobj.TPB = 4
+configobj.padded_TPB = TPB + 2
+configobj.UNEXPLORED = int(math.pow(2, (scale_factor*2)))
+# configobj.UNEXPLORED = 9999999
+configobj.OPEN = 1
+configobj.CLOSED = 0

acecb7a1a2027ee49210a402800a3defe539e2e5
Luna Dilangalen <dadilangalen@gmail.com>
2021-01-09 13:03:16 +0800

    adding heuristic argument to main

--- implementation_v2/main.py
+++ implementation_v2/main.py
@@ -32,11 +32,12 @@ def main():
     from_file.close()
     to_file.close()
     
-
     parser = argparse.ArgumentParser(description='CPU vs GPU Pathfinding')
     parser.add_argument('scale_factor', type=int, help='Scale factor (power of 2)')
     parser.add_argument('TPB', type=int, help='Block width')
     parser.add_argument('complexity', type=str, help='Map Complexity')
+    parser.add_argument('heuristics', type=str, help='Heuristic')
+    
     # parser.add_argument('seed', type=int, help='RNG Seed', default=config.seed)
     parser.add_argument('runs', type=int, help='Test run count', default=100)
     args = parser.parse_args()
@@ -46,94 +47,68 @@ def main():
     config.dim = int(math.pow(2, config.scale_factor)), int(math.pow(2, config.scale_factor))
     config.UNEXPLORED = int(math.pow(2, (config.scale_factor*2)))
     complexity = args.complexity
+    heuristics = args.heuristics
     runs = args.runs
     # config.seed = args.seed
 
     possible_scale_factors = list(range(4,11))
     possible_TPBs = [4,8,16]
 
-    # for _scale_factor in possible_scale_factors:
-    #     for _TPB in possible_TPBs:
-    #         config.scale_factor = _scale_factor
-    #         config.TPB = _TPB
-    #         config.padded_TPB = config.TPB + 2
-    #         config.dim = int(math.pow(2, config.scale_factor)), int(math.pow(2, config.scale_factor))
-    #         config.UNEXPLORED = int(math.pow(2, (config.scale_factor*2)))
-    #         print('===== Experiment Setup =====')
-    #         print('Grid dimensions:', config.dim, 'Kernel Block Width:', (config.TPB, config.TPB), 'Padded Block Width:', (config.padded_TPB,config.padded_TPB), 'Max Value:', config.UNEXPLORED)
-    #         print('===== Testing CPU vs GPU Pathfinding Approach for %d runs =====' %(runs))
+    print(heuristics)
+    # for i in range(runs):
+    #     print('===== %dth Test =====' %(i))
+    #     width, height = config.dim
+    #     # test_func()
 
-    for i in range(runs):
-        print('===== %dth Test =====' %(i))
-        width, height = config.dim
-        # test_func()
+    #     print('----- Preparing Grid -----')
+    #     # create grid from image dataset
+    #     grid = np.zeros(config.dim, dtype=np.int32)
+    #     # helper.createGridFromDatasetImage('dataset/select-maps/simplest', grid, config.dim)
+    #     image = helper.createGridFromDatasetImage('dataset/select-maps/%s'%(complexity), grid, config.dim)
+    #     # grid = np.ones(config.dim, dtype=np.int32)
 
-        print('----- Preparing Grid -----')
-        # create grid from image dataset
-        grid = np.zeros(config.dim, dtype=np.int32)
-        # helper.createGridFromDatasetImage('dataset/select-maps/simplest', grid, config.dim)
-        image = helper.createGridFromDatasetImage('dataset/select-maps/%s'%(complexity), grid, config.dim)
-        # grid = np.ones(config.dim, dtype=np.int32)
+    #     # generate random start and goal
+    #     start = [-1, -1]
+    #     goal = [-1, -1]
+    #     helper.randomStartGoal(grid, start, goal)
+    #     # start = [0, 0]
+    #     # goal = [grid.shape[0]-1, grid.shape[1]-1]
+    #     start = np.array(start)
+    #     goal = np.array(goal)
+    #     start_1d_index = start[0]*width+start[1]
+    #     goal_1d_index = goal[0]*width+goal[1]
 
-        # generate random start and goal
-        start = [-1, -1]
-        goal = [-1, -1]
-        helper.randomStartGoal(grid, start, goal)
-        # start = [0, 0]
-        # goal = [grid.shape[0]-1, grid.shape[1]-1]
-        start = np.array(start)
-        goal = np.array(goal)
-        start_1d_index = start[0]*width+start[1]
-        goal_1d_index = goal[0]*width+goal[1]
+    #     print(grid)
+    #     print(start)
+    #     print(goal)
 
-        print(grid)
-        print(start)
-        print(goal)
+    #     helper.drawGrid(grid, tuple(start), tuple(goal))
 
-        helper.drawGrid(grid, tuple(start), tuple(goal))
+    #     # cpu implementation
+    #     runs_cpu, time_ave_cpu, path_cpu = cpu.test(grid, start, goal)
+    #     path_length_cpu = len(path_cpu)
+    #     path_exists_cpu = path_cpu[0] == start_1d_index and path_cpu[-1] == goal_1d_index
+    #     # gpu implementation
+    #     runs_gpu, time_ave_gpu, path_gpu = gpu.test(grid, start, goal)
+    #     path_length_gpu = len(path_gpu)
+    #     path_exists_gpu = path_length_gpu > 0
+    #     # summarize info
+    #     print('----- Summary -----')
+    #     # general info
+    #     print('Image used:', image)
+    #     print('Start:', start)
+    #     print('Goal:', goal)
+    #     print()
+    #     # cpu info
+    #     print('search runtime (CPU):', time_ave_cpu )
+    #     print('path length (CPU):', len(path_cpu))
+    #     print()
+    #     print('search runtime (GPU):', time_ave_gpu)
+    #     print('path length (GPU):', path_length_gpu)
+    #     print()
 
-        # cpu implementation
-        runs_cpu, time_ave_cpu, path_cpu = cpu.test(grid, start, goal)
-        path_length_cpu = len(path_cpu)
-        path_exists_cpu = path_cpu[0] == start_1d_index and path_cpu[-1] == goal_1d_index
-        # gpu implementation
-        # store all relevant info about gpu implementation for 3 different TPB configs (4, 8, 16)
-        # time_ave_gpu_list = []
-        # path_length_gpu_list = []
-        # path_exists_gpu_list = []
-
-        # for _TPB in possible_TPBs:
-        #     config.TPB = _TPB
-        #     config.padded_TPB = config.TPB + 2
-
-        runs_gpu, time_ave_gpu, path_gpu = gpu.test(grid, start, goal)
-        path_length_gpu = len(path_gpu)
-        path_exists_gpu = path_length_gpu > 0
-
-        # time_ave_gpu_list.append(time_ave_gpu)
-        # path_length_gpu_list.append(path_length_gpu)
-        # path_exists_gpu_list.append(path_exists_gpu)
-
-
-        # summarize info
-        
-
-        print('----- Summary -----')
-        # general info
-        print('Image used:', image)
-        print('Start:', start)
-        print('Goal:', goal)
-        print()
-        # cpu info
-        print('search runtime (CPU):', time_ave_cpu )
-        print('path length (CPU):', len(path_cpu))
-        print()
-        print('search runtime (GPU):', time_ave_gpu)
-        print('path length (GPU):', path_length_gpu)
-        print()
-
-        with open(os.path.join(os.getcwd(), 'implementation_v2/metrics/data/performance.csv'), "a") as log_file:
-            log_file.write("{},{},{},{},{},{},{},{},{},{},{}\n".format(image, width, config.TPB, start_1d_index, goal_1d_index, time_ave_cpu, time_ave_gpu, path_length_cpu, path_length_gpu, path_exists_cpu, path_exists_gpu))
+    #     with open(os.path.join(os.getcwd(), 'implementation_v2/metrics/data/performance.csv'), "a") as log_file:
+    #         log_file.write("{},{},{},{},{},{},{},{},{},{},{}\n".format(image, width, config.TPB, start_1d_index, goal_1d_index, time_ave_cpu, time_ave_gpu, path_length_cpu, path_length_gpu, path_exists_cpu, path_exists_gpu))
 
 
 
